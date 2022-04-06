b2bf54707c3b154fef769a60703f5b1241e40982
Daniel Lipniacki <daniel.lipniacki.stud@pw.edu.pl>
2022-04-06 17:55:18 +0200

    first 4 locations

--- prolog/adventure.pl
+++ prolog/adventure.pl
@@ -4,34 +4,127 @@
 games fackts
 inventory
 
- 
- 
- 
+interact 
+talk
+invetory
+kup piwo
+
+
  */
 :- dynamic i_am_at/1, at/2, holding/1.
 :- retractall(at(_, _)), retractall(i_am_at(_)), retractall(alive(_)), retractall(has(_)).
 
-/* things to implement */
-
-i_am_at(someplace).
-i_am_at(pole).
-
-path(someplace, n, pole).
-
-at(thing, someplace).
-
-at(cos, someplace).
-at(kubek, someplace).
-
 
 /* These rules describe the various rooms.  Depending on
    circumstances, a room may have more than one description. */
+/* things to implement */
 
-describe(someplace) :- write('You are someplace.'), nl.
-describe(pole) :- write('You are at pole.'), nl.
+/* lokacja 1 - swiętyDąb */
+i_am_at(swiętyDąb).
 
-describe(kubek) :- write('KUBEK, staty: .'), nl.
-describe(lll) :- write('lll, staty: .'), nl.
+describe(swiętyDąb) :- write("
+Gerwant z Riviery podróżując szlakiem dociera do Świętego Dębu. Niestety te sakramentalne miejsce zostało zbrukane ludzką krwią.
+
+Zabójca potworów może udać się na:
+- północ
+"), nl.
+
+describe(cialo) :- write("Analiza Wiedźmina: Rany od szponów i kłów"), nl.
+describe(totem) :- write("Analiza Gerwanta z Riviery: Totem został zbudowany ze szczątek i poroża jelenia. To wygląda na ostrzeżenie."), nl.
+
+chat(wiesniak) :- write("Wieśniak jest sparaliżowane strachem i majaczy"), nl.
+
+at(cialo,swiętyDąb).
+at(totem,swiętyDąb).
+
+be(wiesniak ,swiętyDąb).
+
+path(swiętyDąb, n, wieśGrobla).
+path(swiętyDąb, n, wieśGrobla).
+forbiddenMove(polanaKoloChatyDrwali).
+
+/* lokacja 2 - Wieś Grobla */
+
+describe(wieśGrobla) :- write("
+Gerwant dociera do Wsi Grobla.
+
+Wiedźmak może udać się do:
+- dom Sołtysa (komenda odwiedzSołtysa)
+- karczma (komenda wejdzDoKarczmy)
+"), nl.
+
+describe(studnia) :- write("Znalezienie miecza *Gnomski Gwyhyr*"), nl.
+describe(tablicaOgłoszeń) :- write("Gerwant znajduje Zlecenie na *Potwora Lasu* z podpisem *sołtys* wsi Grobla"), nl.
+
+at(studnia, wieśGrobla).
+at(tablicaOgłoszeń, wieśGrobla)
+
+path(wieśGrobla, domSołtysa,domSołtysa).
+path(wieśGrobla, karczma, karczma).
+
+/* lokacja 3 - Dom sołtysa */
+
+describe(domSołtysa) :- write("
+Zabójca potworów wchodzi do domu sołtysa
+
+Gerwant z Riviery może udać się na:
+- zewnątrz (komenda wyjdz)
+"), nl.
+
+be(sołtys, domSołtysa).
+
+chat(sołtys) :- write("Gerwant z Riviery pyta o zlecenie na potwora. Sołtys opowiada mu o atakach potwora w lasach na południe od wioski. Proponuje porozmawiać z *ocalałymi drwalami*, przesiadującymi w karczmie i spytać ich o *atak*"), nl.
+
+path(domSołtysa, wyjdz, wieśGrobla).
+
+/* lokacja 4 - Wieś Grobla */
+
+describe(karczma) :- write("
+Gerwant wchodzi do Karczmy
+
+Możesz kupic piwo(komenda buy_beer)
+
+Rivierijczyk może wyjść na:
+- zewnątrz (komenda wyjdz)
+"), nl.
+
+be(drwale, karczma).
+be(karczmarz, karczma).
+
+chat(drwale) :- write("Grupa drwali odpowiada, że zobaczyła wysokiego potwora z drewna i kości. Następnie zaatakowały ich wilki. Zostali zaatakowani koło Chaty drwali na *wschód* od Świętego Dębu"), retract(forbiddenMove(polanaKoloChatyDrwali)) nl.
+chat(karczmarz) :- write("Pordóżniku, zapraszam na piwo"), nl.
+
+path(karczmarz, wyjdz, wieśGrobla).
+
+
+
+
+
+
+
+
+in_inventory(kubek) :- write("KUBEK, staty: ."), nl.
+in_inventory(lll) :- write("lll, staty: ."), nl.
+
+
+
+interact(X) :-
+        i_am_at(Place),
+        at(X, Place),
+        describe(X).
+
+talk(X) :-
+        i_am_at(Place),
+        be(X, Place),
+        chat(X).
+
+buy_beer :-
+        i_am_at(karczma),
+        has(mieszek),
+        write("Gerwant pije piwo").
+
+buy_beer :-
+        write("Nie masz tyle hajsu").
 
 /* These rules describe how to pick up an object. */
 
@@ -78,8 +171,19 @@ e :- go(e).
 
 w :- go(w).
 
+wejdzDoKarczmy :- go(karczma).
+
+odwiedzSołtysa :- go(domSołtysa).
+
+wyjdz :- go(wyjdz).
 
 /* This rule tells how to move in a given direction. */
+
+go(Direction) :-
+        i_am_at(Here),
+        path(Here, Direction, There),
+        forbiddenMove(There),
+        write('Nie mozesz tam iść').
 
 go(Direction) :-
         i_am_at(Here),
@@ -89,7 +193,7 @@ go(Direction) :-
         !, look.
 
 go(_) :-
-        write('You can''t go that way.').
+        write('Nie mozesz tam iść').
 
 
 /* This rule tells how to look about you. */
@@ -99,6 +203,7 @@ look :-
         describe(Place),
         nl,
         notice_objects_at(Place),
+        notice_people_at(Place),
         nl.
 
 
@@ -107,19 +212,26 @@ look :-
 
 notice_objects_at(Place) :-
         at(X, Place),
-        write('There is a '), write(X), write(' here.'), nl,
+        write('Wiedźmin zauważa:'), write(X), write(' tutaj.'), nl,
         fail.
 
 notice_objects_at(_).
+
+notice_people_at(Place) :-
+        be(X, Place),
+        write('Wiedźmin może pogadać z: '), write(X), write(' tutaj.'), nl,
+        fail.
+
+notice_people_at(_).
 
 
 /* inventory */
 
 list_items :-
-        forall(has(X), describe(X)).
+        forall(has(X), in_inventory(X)).
 
 
-add_item(X) :-
+inventory(X) :-
         i_am_at(Place),
         at(X, Place),
         retract(at(X, Place)),
@@ -136,8 +248,8 @@ remove_item(X) :-
         !, nl.
 
 /*FIXME!!!!!!!: if holds item */
-equip_item(X) :-
-        holding(Item),
+equip_item(_) :-
+        holding(item),
         write('You are holding an item.'),
         !, nl.
 
